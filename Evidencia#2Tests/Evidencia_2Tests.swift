ddfc63c2e10f1487fc0a5b110f3775103c954381
Itzcoatl Torres <AL03004004@tecmilenio.mx>
2024-04-18 11:06:48 -0600

    Evidencia Itzcoatl

---
+++ Evidencia#2Tests/Evidencia_2Tests.swift
@@ -0,0 +1,22 @@
+
+
+import XCTest
+@testable import Evidencia_2
+
+final class Evidencia_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
